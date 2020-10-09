7607a0a636c729493e519653ce720ccb81c53c14
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 19:43:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function updateObjectWithKeyAndValue(object, key, value){
+
+    return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key){
+  delete object.key
+   return objects
+
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+
+}
