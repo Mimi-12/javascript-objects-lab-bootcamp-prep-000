941d41e3cdcd2dc0ff418682222aacd7b68ac80f
selam lemma <oneiconforlife26@gmail.com>
2020-10-09 19:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function deleteFromObjectByKey(object, key){
 return delete object.key
 }
 function destructivelyDeleteFromObjectByKey(object, key){
- delete object.key
+ delete object[key]
+ return object
 }
